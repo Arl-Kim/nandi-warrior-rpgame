55e692e57cb6a11f1ea6be0a9ab7bd87a7fb8aa3
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-01-30 11:32:09 +0300

    Create js file and initialize variables

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let experience = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let enemyHealth;
+let inventory = ["kuriot(stick)"];
+
+const shopButton = document.querySelector("#button1");
+const forestButton = document.querySelector("#button2");
+const fightButton = document.querySelector("#button3");

3da187a0ecce4ec1221951dd7c815b6d26e60a20
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-01-31 12:13:06 +0300

    Add some basic interactivity
    Initialize more variables and define functions to facilitate basic user navigation using the button controls

--- script.js
+++ script.js
@@ -11,3 +11,57 @@ let inventory = ["kuriot(stick)"];
 const shopButton = document.querySelector("#button1");
 const forestButton = document.querySelector("#button2");
 const fightButton = document.querySelector("#button3");
+const interactiveText = document.querySelector("#text");
+const experienceText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const enemyStats = document.querySelector("#enemyStats");
+const enemyName = document.querySelector("#enemyName");
+const enemyHealthText = document.querySelector("#enemyHealth");
+
+//Initialize Buttons
+shopButton.onclick = visitShop;
+forestButton.onclick = goToForest;
+fightButton.onclick = fightEnemy;
+
+function goSeers(){
+    shopButton.innerText = "Visit Supplies Shop";
+    forestButton.innerText = "Go To Forest";
+    fightButton.innerText = "Fight Enemy";
+
+    
+    shopButton.onclick = visitShop;
+    forestButton.onclick = goToForest;
+    fightButton.onclick = fightEnemy;
+
+    interactiveText.innerText = "You visit the seers in their secret chambers. After you recover from the draining effects of journeying into the Seers Lair, Orkoiyot The Great approaches you...He exclaims! 'What You Seek You Will Not Find Here. The Threat Is Still Imminent, Go Find It Before It Finds Us. You Can Find Supplies In The Shop, Trouble & Peace In The Forest! You Alone Know The Path You Must Choose, Ultimate Nandi Warrior!'";
+}
+
+function visitShop(){
+    shopButton.innerText = "Trade 10 Gold For 10 Health";
+    forestButton.innerText = "Trade 30 Gold For A Weapon";
+    fightButton.innerText = "Go See The Seers";
+
+    
+    shopButton.onclick = buyHealth;
+    forestButton.onclick = buyWeapon;
+    fightButton.onclick = goSeers;
+
+    interactiveText.innerText = "You walk into the shop...What do you do next ?";
+}
+
+function goToForest(){
+    console.log("You have decided to seek guidance in nature. You walk into the forest...");
+}
+
+function fightEnemy(){
+    console.log("The time is now! destroy your enemy!")
+}
+
+function buyHealth(){
+
+}
+
+function buyWeapon(){
+
+}

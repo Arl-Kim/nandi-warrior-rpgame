dac4c852b56e0fc3a6a8eaed1e02291e5686d079
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-02-01 10:38:43 +0300

    Add an Array locations and a function to update the location of a player

--- script.js
+++ script.js
@@ -19,10 +19,28 @@ const enemyStats = document.querySelector("#enemyStats");
 const enemyName = document.querySelector("#enemyName");
 const enemyHealthText = document.querySelector("#enemyHealth");
 
+const locations = [{
+    name: "Seers Lair",
+    "button text": ["Visit Supplies Shop", "Go To Forest", "Fight Enemy"],
+    "button functions": [visitShop, goToForest, fightEnemy],
+    text: "You visit the seers in their secret chambers. After you recover from the draining effects of journeying into the Seers Lair, Orkoiyot The Great approaches you...He exclaims! 'What You Seek You Will Not Find Here. The Threat Is Still Imminent, Go Find It Before It Finds Us. You Can Find Supplies In The Shop, Trouble & Peace In The Forest! You Alone Know The Path You Must Choose, Ultimate Nandi Warrior!'"
+},
+{
+    name: "Supplies Shop",
+    "button text": ["Trade 10 Gold For 10 Health", "Trade 30 Gold For A Weapon", "Go See The Seers"],
+    "button functions": [buyHealth, buyWeapon, goSeers],
+    text: "You walk into the shop...What do you do next ?"
+}
+];
+
 //Initialize Buttons
 shopButton.onclick = visitShop;
 forestButton.onclick = goToForest;
 fightButton.onclick = fightEnemy;
+
+function update(location){
+
+}
 
 function goSeers(){
     shopButton.innerText = "Visit Supplies Shop";
